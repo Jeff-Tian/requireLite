2b2c352741150c6a0ac890e6dae0295288376dd9
Jeff Tian <Jeff.Tian@EF.com>
2014-01-28 15:04:24 +0800

    Add requireLite.js and a basic usage example.

---
+++ Web.UI.Scripts.Tests/spec/root/requireLite.Spec.js
@@ -0,0 +1,54 @@
+﻿///<reference path="/../requireLite.js"/>
+///<reference path="/lib/jasmine/jasmine.js"/>
+
+describe("requireLite helper test suite", function () {
+    it("Spec: Can get the module name from a url path", function() {
+        expect(requireLiteHelper.getModuleNameFromPath("http://localhost:33109/Scripts/2012.2.607/telerik.timepicker.min.js")).toBe("telerik.timepicker");
+        expect(requireLiteHelper.getModuleNameFromPath("http://localhost:33109/Scripts/2012.2.607/telerik.timepicker.js")).toBe("telerik.timepicker");
+        expect(requireLiteHelper.getModuleNameFromPath("window")).toBe("window");
+    });
+
+    it("Spec: Can detect if a module has been loaded already", function() {
+        expect(requireLiteHelper.hasBeenLoadedAlready("window")).toBe(true);
+        expect(requireLiteHelper.hasBeenLoadedAlready("not.loaded")).toBe(false);
+    });
+});
+
+(function () {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 250;
+
+    /**
+     Create the `HTMLReporter`, which Jasmine calls to provide results of each spec and each suite. The Reporter is responsible for presenting results to the user.
+     */
+    var htmlReporter = new jasmine.HtmlReporter();
+    jasmineEnv.addReporter(htmlReporter);
+
+    /**
+     Delegate filtering of specs to the reporter. Allows for clicking on single suites or specs in the results to only run a subset of the suite.
+     */
+    jasmineEnv.specFilter = function (spec) {
+        return htmlReporter.specFilter(spec);
+    };
+
+    /**
+     Run all of the tests when the page finishes loading - and make sure to run any previous `onload` handler
+  
+     ### Test Results
+  
+     Scroll down to see the results of all of these specs.
+     */
+    var currentWindowOnload = window.onload;
+    window.onload = function () {
+        if (currentWindowOnload) {
+            currentWindowOnload();
+        }
+
+        document.querySelector('.version').innerHTML = jasmineEnv.versionString();
+        execJasmine();
+    };
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+})();
---
+++ requireLite.js
@@ -0,0 +1,140 @@
+﻿;
+(function() {
+    if (!window.requireLite) {
+        
+        function getModuleNameFromPath(path) {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="path" type="String">
+            /// for example: http://localhost:33109/Scripts/2012.2.607/telerik.timepicker.min.js
+            /// </param>
+            /// <returns type="String">
+            /// for example: telerik.timepicker
+            /// </returns>
+            var startIndex = path.lastIndexOf("/");
+            if (startIndex < 0) {
+                startIndex = path.lastIndexOf("\\");
+            }
+
+            var endIndex = path.lastIndexOf(".");
+            if (endIndex < 0) {
+                endIndex = path.lastIndexOf("-");
+            }
+            if (endIndex < 0) {
+                endIndex = path.lastIndexOf("_");
+            }
+            endIndex = endIndex < 0 ? path.length : endIndex;
+
+            var moduleName = path.substring(startIndex + 1, endIndex);
+
+            return moduleName.replace(".min", "").replace(".dev", "").replace("-vsdoc", "");
+        }
+        
+        function getModule(moduleObj) {
+            var module = null;
+            
+            if ((typeof moduleObj === "string") && moduleObj !== "") {
+                module = {
+                    name: getModuleNameFromPath(moduleObj),
+                    path: moduleObj
+                };
+            } else if ((typeof moduleObj === "object") && moduleObj !== null) {
+                try {
+                    if (moduleObj.name.length > 0 && moduleObj.path.length > 0) {
+                        module = {
+                            name: moduleObj.name,
+                            path: moduleObj.path
+                        };
+                    }
+                } catch(ex) {
+                    module = null;
+                }
+            }
+
+            return module;
+        }
+        
+        function hasBeenLoadedAlready(moduleObj) {
+            /// <summary>
+            /// Detect if a module has been loaded already
+            /// </summary>
+            /// <param name="moduleObj" type="Object|String">
+            /// for example: {path: "http://localhost:33109/Scripts/jquery.js", name: "$"}
+            /// or "http://localhost:33109/Scripts/2012.2.607/telerik.timepicker.js".
+            /// </param>
+            /// <returns type="Boolean">
+            /// </returns>
+            var m = getModule(moduleObj);
+            
+            if (m === null) {
+                throw "Invalid argument 'moduleObj'. Expected: <An object or a string>; Actual: <" + moduleObj + ">";
+            }
+            
+            try {
+                var module = eval(m.name);
+
+                return typeof module !== "undefined";
+            } catch(ex) {
+                return false;
+            }
+        }
+
+        function loadScript(path, callback) {
+            var module = getModule(path);
+            
+            if (module === null) {
+                throw "The argument 'path' is not valid. Expect: <a valid script path> or <an object: {path: 'a valid script path', name: 'module name'}>; Actual: <" + path + ">";
+            }
+
+            if (!hasBeenLoadedAlready(module)) {
+                var script = document.createElement("script");
+                script.async = false;
+                script.src = module.path;
+                script.type = "text/javascript";
+                if (typeof callback === "function") {
+                    script.onload = callback;
+                }
+
+                (document.getElementsByTagName('HEAD')[0] || document.body).appendChild(script);
+            } else {
+                if (typeof callback === "function") {
+                    callback();
+                }
+            }
+        }
+
+        function loadScripts(paths, callback) {
+            loadScript(paths.shift(), getCallback(paths, callback));
+        }
+
+        function getCallback(paths, finalCallback) {
+            if ((paths instanceof Array) && paths.length > 0) {
+                return function() { loadScripts(paths, finalCallback); };
+            } else {
+                return finalCallback;
+            }
+        }
+
+        function requireLite(dependentScriptPaths, callback) {
+            
+            if (typeof dependentScriptPaths === "string") {
+                dependentScriptPaths = [dependentScriptPaths];
+            }
+
+            if (!(dependentScriptPaths instanceof Array)) {
+                throw "Argument 'dependentScriptPaths' should be an array of strings or {path: 'module path', name: 'module name'}s.";
+            }
+
+            // Clone a copy of dependentScriptPaths
+            var paths = dependentScriptPaths.slice(0);
+            loadScripts(paths, callback);
+        }
+
+        window.requireLite = requireLite;
+        window.requireLiteHelper = {            
+            getModuleNameFromPath: getModuleNameFromPath,
+            hasBeenLoadedAlready: hasBeenLoadedAlready
+        };
+    }
+})();
